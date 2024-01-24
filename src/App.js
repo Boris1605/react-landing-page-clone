a6f992ef706907f9d11f383c39ddaf960a73ac1c
Boris Djuric <boris_djuric@hotmail.com>
2024-01-24 20:28:03 +0100

    Apply scss styling

--- src/App.js
+++ src/App.js
@@ -6,54 +6,65 @@ export default function App() {
     <div className={styles.app}>
       <div className={styles.wrapper}>
         <header className={styles.header}>
-          <div className="Logo" />
-          <nav>
-            <ul>
-              <li>
-                <a href="/">About</a>
-              </li>
-              <li>
-                <a href="/">Pricing</a>
-              </li>
-              <li>
-                <a href="/">Use Case</a>
-              </li>
-            </ul>
+          <div className={styles.leftContent}>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">About</a>
+                </li>
+                <li>
+                  <a href="/">Pricing</a>
+                </li>
+                <li>
+                  <a href="/">Use Case</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.logo}>
             <img
               src="https://assets-global.website-files.com/635dda7647d1612d7d877c36/64b9a8b87d7ee6f947910e82_rewind-icon.svg"
               alt="Logo"
             />
-            <img
-              src="https://assets-global.website-files.com/635dda7647d1612d7d877c36/64c088267e8881a7472517a4_rewind-logotype.svg"
-              alt="LogoText"
-            />
-          </nav>
+          </div>
           <nav>
-            <a href="/" className="button">
+            <a href="/" className={`button ${styles.getRewindLink}`}>
               Get Rewind
             </a>
           </nav>
         </header>
         <main>
-          <section>
-            <div>Coming soon to Windows!</div>
-            <h1>Your AI assistant that has all the context</h1>
+          <section className={styles.middleSection}>
+            <div
+              className={`${styles.comingSoonText} ${styles.comingSoonButton}`}
+            >
+              Coming soon to Windows!
+            </div>
+
+            <h1>
+              Your AI assistant
+              <br /> that has all the
+              <br /> context
+            </h1>
             <p>
-              Rewind is a personalized AI powered by everything you’ve seen,
-              said, or heard. Your colleagues will wonder how you do it all.
+              Rewind is a personalized AI powered by everything
+              <br /> you've seen, said, or heard. Your colleagues will
+              <br /> wonder how you do it all.
             </p>
-            <input placeholder="Enter your email" type="email" />
-            <a href="/">Get Rewind</a>
+            <div className="inputContainer">
+              <input placeholder="Enter your email" type="email" />
+              <button className={styles.getRewindButton}>Get Rewind</button>
+            </div>
           </section>
-          <section>
+          {/* <section>
             <div>
               <img src="/" alt="Demovideo" />
             </div>
-          </section>
-          <section>
+          </section> */}
+          {/* <section>
             <h2>Professionals use Rewind to get more done, faster.</h2>
-          </section>
-          <section></section>
+          </section> */}
+          {/* <section></section> */}
         </main>
         <footer />
       </div>
